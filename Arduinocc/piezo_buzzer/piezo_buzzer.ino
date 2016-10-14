617f120fc52704b76bd0d6aeb607aee051c0910e
poolpeau <tongkwai.l@gmail.com>
2016-10-13 20:21:51 -0400

    HOMEWORK WEEK 7

---
+++ Arduinocc/piezo_buzzer/piezo_buzzer.ino
@@ -0,0 +1,33 @@
+int button = 5;
+int buzzer =  3;
+int buttonValue = 0;
+//int tone = random(100,200);
+
+void setup() {
+  // put your setup code here, to run once:
+
+ Serial.begin(9600); 
+pinMode( button, INPUT);
+pinMode(buzzer, OUTPUT);
+
+ 
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  
+buttonValue = digitalRead(button);
+if (buttonValue == HIGH) {
+  digitalWrite(buzzer,HIGH);
+    tone(3,100,10);
+
+
+  }else {
+   
+  digitalWrite(buzzer, LOW);
+  }
+
+}
+
